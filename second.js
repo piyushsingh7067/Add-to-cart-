fd5d3d390555ea44f79af537fb1e50696816f6d2
Piyush singh <piyushsingh@gmail.com>
2025-03-29 15:30:22 +0530

    last

--- second.js
+++ second.js
@@ -1,46 +1,46 @@
-// function test1(){
-//     const p1 = new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             resolve("first promise resolve");
-//         },3000)
-//     })
-//     return p1;
-// }
+function test1(){
+    const p1 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("first promise resolve");
+        },3000)
+    })
+    return p1;
+}
 
-// function test2(){
-//     const p2 = new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             resolve("second promise resolve");
-//         },4000)
-//     })
-//       return p2;    
-// }
+function test2(){
+    const p2 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("second promise resolve");
+        },4000)
+    })
+      return p2;    
+}
 
 
-// async function greet() {
-//   //  console.log("Hello I greet you");
+async function greet() {
+  //  console.log("Hello I greet you");
 
-//     const [data1,data2] = await Promise.all([test1(),test2()]);          // multiple code ko parallaly ek sath execute
-//     console.log(data1);
-//     console.log(data2);
-// }
-// greet();
+    const [data1,data2] = await Promise.all([test1(),test2()]);          // multiple code ko parallaly ek sath execute
+    console.log(data1);
+    console.log(data2);
+}
+greet();
  
 
 
 
 
-// async function greet() {
+async function greet() {
 
-//     console.log("Hello I greet you");
-//     const data1 = await test1();
-//     console.log(data1);
+    console.log("Hello I greet you");
+    const data1 = await test1();
+    console.log(data1);
 
-//     const data2 = await test2();
-//     console.log(data2);
-// }
-// greet();
+    const data2 = await test2();
+    console.log(data2);
+}
+greet();
 
-// console.log("Hello coder army");
-// console.log("kaise ho app sab log");
-// console.log("sab thik"); 
+console.log("Hello coder army");
+console.log("kaise ho app sab log");
+console.log("sab thik"); 
